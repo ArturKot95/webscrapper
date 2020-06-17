0791cb5b5c8c22b6b7728c59d5d8bf41a58f6829
Artur <arturkot95@gmail.com>
2020-06-17 18:36:36 +0200

    Get cookbooks titles and upload to database

--- index.js
+++ index.js
@@ -31,6 +31,33 @@ async function run() {
   await page.waitForNavigation({
     waitUntil: 'networkidle0'
   });
+
+  await page.goto('https://allegro.pl/kategoria/kuchnia-potrawy-79205');
+  
+  let articles = await page.evaluate(() => {
+    let result = [];
+
+    document.querySelectorAll('article').forEach(article => {
+      result.push({
+        title: article.querySelector('h2 a').innerHTML
+      });
+    });
+
+    return result;
+  });
+  
+  let articlesReduced = articles.reduce((result, article, index, articles) => {
+    return result + `(${sqlstring.escape(article.title)})${index === articles.length - 1 ? '': ','}`;
+  }, '');
+
+  let sqlQuery = `INSERT INTO cookbooks (title) VALUES ${articlesReduced}`;
+
+  connection.query(sqlQuery, (error, result, fields) => {
+    if (error) throw error;
+    console.log('Uploaded titles to database');
+  });
+
+  connection.end();
 }
 
 connection.connect((error) => {
